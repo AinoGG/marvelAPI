63dbed81875bd60ed66ff5ac1bcd8daa7433d8ed
AinoGG <ryadomgg@gmail.com>
2022-03-11 15:48:38 +0300

    dynamic path add

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,7 +4,6 @@ const useMarvelService = () => {
     const {loading, request, error, clearError} = useHttp();
 
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
     const _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62';
     const _baseOffset = 210;
 
@@ -23,7 +22,7 @@ const useMarvelService = () => {
         return res.data.results.map(_transformComics);
     }
 
-    const getComics = async (id) => {
+    const getComic = async (id) => {
         const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
         return _transformComics(res.data.results[0]);
     }
@@ -52,7 +51,7 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getCharacters, getAllComics, getComics}
+    return {loading, error, clearError, getAllCharacters, getCharacters, getAllComics, getComic}
 }
 
 export default useMarvelService;
